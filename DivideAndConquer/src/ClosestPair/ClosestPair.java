182a7f51de77e4c37b1654fbaba8fdb9cc535a60
Dean <d.v.q.nguyen@protonmail.com>
2021-10-02 16:00:42 +0200

    Create template for ClosestPair assignment

---
+++ DivideAndConquer/src/ClosestPair/ClosestPair.java
@@ -0,0 +1,18 @@
+package ClosestPair;
+
+import java.util.List;
+
+public class ClosestPair {
+    /**
+     * Takes a list of points and returns the distance between the closest pair.
+     * This is done with divide and conquer.
+     *
+     * @param points
+     *     - list of points that need to be considered.
+     * @return smallest pair-wise distance between points.
+     */
+    public static double closestPair(List<Point> points) {
+        // TODO
+        return 0;
+    }
+}
