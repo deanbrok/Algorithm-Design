f712b7f3187521f173a08d068c943f5c40fa3c3b
Dean <d.v.q.nguyen@protonmail.com>
2021-10-17 09:16:47 +0200

    Revised OptimalWorkstation

--- Greedy/src/OptimalWorkstation/OptimalWorkstation.java
+++ Greedy/src/OptimalWorkstation/OptimalWorkstation.java
@@ -12,64 +12,82 @@ public class OptimalWorkstation {
      * @return the number of unlocks that can be saved.
      */
     int solve(int n, int m, int[] start, int[] duration) {
-        Interval[] requests = new Interval[n];
-        for (int i = 1; i <= n; i++) {
-            requests[i - 1] = new Interval(start[i], start[i] + duration[i]);
-        }
-
-        Arrays.sort(requests);
-        PriorityQueue<Interval> stations = new PriorityQueue<>(new EndComparator());
+        Interval[] intervals = parseIntervals(n, start, duration);
+        Arrays.sort(intervals);
+        PriorityQueue<Interval> workstationPQ = new PriorityQueue<>(Comparator.comparingInt(x -> x.f));
 
         int result = 0;
 
-        for (Interval interval: requests) {
-            while (!stations.isEmpty()) {
-                if (!isCompatibleInterval(stations.peek(), interval)) break;
+        for (Interval interval: intervals) {
+            boolean found = false;
 
-                Interval station = stations.poll();
-                if (!isLocked(station, interval, m)) {
-                    result++;
+            while (!workstationPQ.isEmpty()) {
+                Interval currentWorkstation = workstationPQ.peek();
+
+                if (currentWorkstation.isCompatible(interval)) {
+                    workstationPQ.poll();
+                    if (currentWorkstation.isWithinLocktime(interval, m)) {
+                        result++;
+                        found = true;
+                        workstationPQ.add(interval);
+                        break;
+                    } else {
+                        continue;
+                    }
+                } else {
                     break;
                 }
             }
-            stations.add(interval);
+
+            if (!found) {
+                workstationPQ.add(interval);
+            }
         }
 
         return result;
     }
 
-    private static boolean isCompatibleInterval(Interval i1, Interval i2) {
-        return i1.end <= i2.start;
-    }
-
-    private static boolean isLocked(Interval station, Interval i, int m) {
-        return i.start - station.end > m;
+    private static Interval[] parseIntervals(int n, int[] start, int[] duration) {
+        Interval[] intervals = new Interval[n];
+        for (int i = 1; i <= n; i++) {
+            intervals[i - 1] = new Interval(start[i], start[i] + duration[i]);
+        }
+        return intervals;
     }
 
     private static class Interval implements Comparable<Interval> {
-        int start;
-        int end;
-        public Interval(int start, int end) {
-            this.start = start;
-            this.end = end;
+
+        int s, f;
+
+        public Interval (int start, int finish) {
+            s = start;
+            f = finish;
+        }
+
+        public boolean isCompatible(Interval i) {
+            return this.f <= i.s;
+        }
+
+        public boolean isWithinLocktime (Interval i, int lockTime) {
+            return i.s - this.f <= lockTime;
         }
 
         @Override
         public int compareTo(Interval o) {
-            if (this.start < o.start) return -1;
-            else if (this.start > o.start) return 1;
+            if (this.s < o.s) return -1;
+            else if (this.s > o.s) return 1;
             return 0;
+        }
+
+        @Override
+        public String toString() {
+            return "Interval{" +
+                    "s=" + s +
+                    ", f=" + f +
+                    '}';
         }
     }
 
-    private static class EndComparator implements Comparator<Interval> {
-        @Override
-        public int compare(Interval o1, Interval o2) {
-            if (o1.end < o2.end) return -1;
-            else if (o1.end > o2.end) return 1;
-            return 0;
-        }
-    }
 
     public static void main(String[] args) {
         int n = 5;
